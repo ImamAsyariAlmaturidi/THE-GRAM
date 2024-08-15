5316c1fe94430601ba33591a2608f2d4b9daed92
ImamAsyariAlmaturidi <imamasyari700@gmail.com>
2024-08-15 14:50:43 +0700

    fix

--- client/src/context/socketContext.jsx
+++ client/src/context/socketContext.jsx
@@ -29,13 +29,3 @@ export const SocketProvider = ({ children }) => {
     </SocketContext.Provider>
   );
 };
-
-import React, { createContext, useMemo, useContext } from "react";
-import { io } from "socket.io-client";
-
-const SocketContext = createContext(null);
-
-export const useSocket = () => {
-  const socket = useContext(SocketContext);
-  return socket;
-};
